8fdd386262e9a123a5f675418019957a0b288736
NoCtrlZ <phantomofrotten@gmail.com>
2020-05-06 23:08:28 +0900

    compile success

---
+++ contracts/ICErc20.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.5.12;
+
+contract ICErc20 {
+    address public underlying;
+    function mint(uint256 mintAmount) external returns (uint);
+    function redeemUnderlying(uint256 redeemAmount) external returns (uint);
+    function balanceOfUnderlying(address owner) external returns (uint);
+    function getCash() external view returns (uint);
+    function supplyRatePerBlock() external view returns (uint);
+}
---
+++ contracts/ReentrancyGuard.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.5.12;
+import "./Initializable.sol";
+
+contract ReentrancyGuard is Initializable {
+    // counter to allow mutex lock with only one SSTORE operation
+    uint256 private _guardCounter;
+
+    function initialize() public initializer {
+        // The counter starts at one to prevent changing it from zero to a non-zero
+        // value, which is a more expensive operation.
+        _guardCounter = 1;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        _guardCounter += 1;
+        uint256 localCounter = _guardCounter;
+        _;
+        require(localCounter == _guardCounter, "ReentrancyGuard: reentrant call");
+    }
+
+    uint256[50] private ______gap;
+}
